9d0ddf0cf29cd9bb02392d53aae853e32bbcdfdd
SinuheSSC <159206257+SinuheSSC@users.noreply.github.com>
2026-04-15 08:59:26 -0600

    Add files via upload

---
+++ PRE-PROCESAMIENTO_EN_NLTK/laboratorio_nlp.py
@@ -0,0 +1,9 @@
+import nltk
+
+   # Descarga del tokenizador 'punkt' (modelo para segmentar oraciones y palabras)
+nltk.download('punkt')
+nltk.download('punkt_tab')   
+   # Descarga del corpus de palabras de paro (stopwords)
+nltk.download('stopwords')
+   
+print("Recursos léxicos descargados y entorno configurado correctamente.")
